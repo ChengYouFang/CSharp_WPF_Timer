879f5d47badfde7a923537ab71642017d60e6729
ChengYouFang <s47548081@hotmail.com>
2013-05-15 23:46:25 +0800

    C# WPF Timer

---
+++ Timer/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace Time
+{
+    public partial class MainWindow : Window
+    {
+        private Timer timer;
+        private SynchronizationContext syncContext = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            syncContext = SynchronizationContext.Current;
+            timer = new Timer(Timer_Ticked, null, 0, 1000);
+        }
+
+        private void Timer_Ticked(Object stateInfo)
+        {
+            SendOrPostCallback callBack = delegate(object state)
+            {
+                this.label1.Content = string.Format("{0:T}", DateTime.Now.ToLocalTime());
+            };
+            syncContext.Post(callBack, null);
+        }
+    }
+}
